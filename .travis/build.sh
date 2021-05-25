a6a7094df8ac84b58a6e55b8f3f740da818ace66
Lukáš Král <53821852+im-konge@users.noreply.github.com>
2021-05-25 20:31:42 +0200

    Kafka producer and consumer, checkstyle (#1)
    * kafka producer and consumer, better logging, copied classes (also little bit reworked)
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>
    
    * add travis and makefiles
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>
    
    * fixups and more dockerfiles and makefiles
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>
    
    * use ubi8 as base image
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>
    
    * Jakub's comments
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>
    
    * change name of docker org
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>
    
    * Make build.sh executable
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>
    
    * fix names for jars and change version to 0.1.0-SNAPSHOT
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

---
+++ .travis/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+export PULL_REQUEST=${PULL_REQUEST:-true}
+export BRANCH=${BRANCH:-main}
+export TAG=${TAG:-latest}
+export DOCKER_ORG=${DOCKER_ORG:-strimzici}
+export DOCKER_REGISTRY=${DOCKER_REGISTRY:-docker.io}
+export DOCKER_TAG=$COMMIT
+
+make build
+
+if [ "$PULL_REQUEST" != "false" ] ; then
+  make docker_build
+  echo "Building PR: Nothing to push"
+else
+  if [ "$TAG" = "latest" ] && [ "$BRANCH" != "main" ]; then
+    make docker_build
+    echo "Not in tag or main branch: Nothing to push"
+  else
+    export DOCKER_TAG=$TAG
+    make docker_build
+  fi
+
+fi
