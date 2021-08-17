054559b3ed00d5990423e286ead35b9b2e9ebdd2
Lukas Kral <lukywill16@gmail.com>
2021-08-17 15:40:09 +0200

    indent in build.sh
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- .azure/scripts/build.sh
+++ .azure/scripts/build.sh
@@ -15,18 +15,18 @@ if [ "$BUILD_REASON" == "PullRequest" ] ; then
   make docker_build
   echo "Building Pull Request - nothing to push"
 elif [[ "$BRANCH" != "refs/tags/"* ]] && [ "$BRANCH" != "refs/heads/main" ]; then
-    make docker_build
-    echo "Not in main branch or in release tag - nothing to push"
+  make docker_build
+  echo "Not in main branch or in release tag - nothing to push"
 else
-    if [ "$BRANCH" == "refs/heads/main" ]; then
-        export DOCKER_TAG="latest"
-    else
-        export DOCKER_TAG="${BRANCH#refs/tags/}"
-    fi
+  if [ "$BRANCH" == "refs/heads/main" ]; then
+      export DOCKER_TAG="latest"
+  else
+      export DOCKER_TAG="${BRANCH#refs/tags/}"
+  fi
 
-    make docker_build
+  make docker_build
 
-    echo "In main branch or in release tag - pushing images"
-    docker login -u $DOCKER_USER -p $DOCKER_PASS $DOCKER_REGISTRY
-    make docker_push
+  echo "In main branch or in release tag - pushing images"
+  docker login -u $DOCKER_USER -p $DOCKER_PASS $DOCKER_REGISTRY
+  make docker_push
 fi
