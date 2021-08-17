dab706fda4aef37cf17bfab6127642e4d656c40f
Lukas Kral <lukywill16@gmail.com>
2021-08-17 09:18:02 +0200

    change travis to azure
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- .azure/scripts/build.sh
+++ .azure/scripts/build.sh
@@ -2,23 +2,26 @@
 
 set -e
 
-export DOCKER_ORG=${DOCKER_ORG:-strimzi-test-clients}
+export DOCKER_ORG=${DOCKER_ORG:-strimzi-examples}
 export DOCKER_REGISTRY=${DOCKER_REGISTRY:-quay.io}
 export DOCKER_TAG=$COMMIT
 
+echo "Build reason: ${BUILD_REASON}"
+echo "Source branch: ${BRANCH}"
+
 make build
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
+if [ "$BUILD_REASON" == "PullRequest" ] ; then
   make docker_build
-  echo "Building PR: Nothing to push"
-elif [[ "$TRAVIS_BRANCH" != "refs/tags/"* ]] && [ "$TRAVIS_BRANCH" != "refs/heads/main" ]; then
+  echo "Building Pull Request - nothing to push"
+elif [[ "$BRANCH" != "refs/tags/"* ]] && [ "$BRANCH" != "refs/heads/main" ]; then
     make docker_build
     echo "Not in main branch or in release tag - nothing to push"
 else
-    if [ "$TRAVIS_BRANCH" == "refs/heads/main" ]; then
+    if [ "$BRANCH" == "refs/heads/main" ]; then
         export DOCKER_TAG="latest"
     else
-        export DOCKER_TAG="${TRAVIS_BRANCH#refs/tags/}"
+        export DOCKER_TAG="${BRANCH#refs/tags/}"
     fi
 
     make docker_build
