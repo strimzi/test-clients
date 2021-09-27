05b4ad4095527c7c555a36bc72dd26ac22edb089
Lukáš Král <53821852+im-konge@users.noreply.github.com>
2021-09-27 09:30:45 +0200

    Add Kafka versioning for Kafka clients and update main `kafka.version` to `3.0.0` (#9)
    * add kafka versioning to kafka clients
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>
    
    * change build.sh to use build-images.sh script
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>
    
    * Jakub's comments vol.1
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>
    
    * Jakub's comments
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>
    
    * set default kafka.version to 3.0.0
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>
    
    * Jakub's comment vol.2
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- .azure/scripts/build.sh
+++ .azure/scripts/build.sh
@@ -9,13 +9,11 @@ export DOCKER_TAG=$COMMIT
 echo "Build reason: ${BUILD_REASON}"
 echo "Source branch: ${BRANCH}"
 
-make build
+./docker-images/build-images.sh build
 
 if [ "$BUILD_REASON" == "PullRequest" ] ; then
-  make docker_build
   echo "Building Pull Request - nothing to push"
 elif [[ "$BRANCH" != "refs/tags/"* ]] && [ "$BRANCH" != "refs/heads/main" ]; then
-  make docker_build
   echo "Not in main branch or in release tag - nothing to push"
 else
   if [ "$BRANCH" == "refs/heads/main" ]; then
@@ -23,10 +21,8 @@ else
   else
       export DOCKER_TAG="${BRANCH#refs/tags/}"
   fi
-
-  make docker_build
-
   echo "In main branch or in release tag - pushing images"
   docker login -u $DOCKER_USER -p $DOCKER_PASS $DOCKER_REGISTRY
-  make docker_push
+
+  ./docker-images/build-images.sh
 fi
---
+++ docker-images/build-images.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+KAFKA_VERSIONS=$(cat docker-images/kafka.version)
+KAFKA_MODULES=$(ls -d kafka/*/ | grep -Ev ".*/target/")
+HTTP_MODULES=$(ls -d http/*/ | grep -Ev ".*/target/")
+MODE=${1:-"push"}
+DOCKER_TAG=${DOCKER_TAG:-"latest"}
+MVN_ARGS=${MVN_ARGS:-""}
+
+echo "Building Kafka clients with versions inside kafka.version file"
+echo "Used build mode: $MODE"
+for KAFKA_VERSION in $KAFKA_VERSIONS
+do
+  for KAFKA_MODULE in $KAFKA_MODULES
+  do
+    if [ $MODE = "build" ];
+      then
+        DOCKER_TAG="$DOCKER_TAG-kafka-$KAFKA_VERSION" MVN_ARGS="$MVN_ARGS -Dkafka.version=$KAFKA_VERSION" make build --directory=$KAFKA_MODULE
+      else
+        DOCKER_TAG="$DOCKER_TAG-kafka-$KAFKA_VERSION" MVN_ARGS="$MVN_ARGS-Dkafka.version=$KAFKA_VERSION" make docker_push --directory=$KAFKA_MODULE
+    fi
+  done
+done
+
+for HTTP_MODULE in $HTTP_MODULES
+do
+  if [ $MODE = "build" ];
+  then
+    make build --directory=$HTTP_MODULE
+  else
+    make docker_push --directory=$HTTP_MODULE
+  fi
+done
