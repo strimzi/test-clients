a92dcea73d6aab5589221b68111c29aa5a3a779a
Lukáš Král <53821852+im-konge@users.noreply.github.com>
2021-09-27 11:20:59 +0200

    Fix docker tag when building images (#10)
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- .azure/scripts/build.sh
+++ .azure/scripts/build.sh
@@ -4,10 +4,17 @@ set -e
 
 export DOCKER_ORG=${DOCKER_ORG:-strimzi-test-clients}
 export DOCKER_REGISTRY=${DOCKER_REGISTRY:-quay.io}
-export DOCKER_TAG=$COMMIT
 
 echo "Build reason: ${BUILD_REASON}"
 echo "Source branch: ${BRANCH}"
+
+if [ "$BRANCH" == "refs/heads/main" ]; then
+  export DOCKER_TAG="latest"
+elif [[ "$BRANCH" == "refs/tags/"* ]]; then
+  export DOCKER_TAG="${BRANCH#refs/tags/}"
+else
+  export DOCKER_TAG=$COMMIT
+fi
 
 ./docker-images/build-images.sh build
 
@@ -16,11 +23,6 @@ if [ "$BUILD_REASON" == "PullRequest" ] ; then
 elif [[ "$BRANCH" != "refs/tags/"* ]] && [ "$BRANCH" != "refs/heads/main" ]; then
   echo "Not in main branch or in release tag - nothing to push"
 else
-  if [ "$BRANCH" == "refs/heads/main" ]; then
-      export DOCKER_TAG="latest"
-  else
-      export DOCKER_TAG="${BRANCH#refs/tags/}"
-  fi
   echo "In main branch or in release tag - pushing images"
   docker login -u $DOCKER_USER -p $DOCKER_PASS $DOCKER_REGISTRY
 
