c713f2a7e45c40238444695e6f3ad81300bab8f1
henryZrncik <40673682+henryZrncik@users.noreply.github.com>
2024-04-09 16:15:56 +0200

    add -o --output json, plain option for describe topic (#84)
    * add -o --output json, plain option for describe topic
    
    Signed-off-by: hzrncik <hzrncik@redhat.com>
    
    * describe command refactor
    
    Signed-off-by: hzrncik <hzrncik@redhat.com>
    
    * admin client add describe topic utils
    
    Signed-off-by: hzrncik <hzrncik@redhat.com>
    
    * style fixes
    
    Signed-off-by: hzrncik <hzrncik@redhat.com>
    
    * describe command javadoc added
    
    Signed-off-by: hzrncik <hzrncik@redhat.com>
    
    ---------
    
    Signed-off-by: hzrncik <hzrncik@redhat.com>

--- admin/src/main/java/io/strimzi/arguments/topic/DescribeTopicCommand.java
+++ admin/src/main/java/io/strimzi/arguments/topic/DescribeTopicCommand.java
@@ -5,9 +5,13 @@
 package io.strimzi.arguments.topic;
 
 import io.strimzi.admin.AdminProperties;
+import io.strimzi.models.KafkaTopicDescription;
+import io.strimzi.utils.DescribeTopicsUtils;
+import io.strimzi.utils.OutputFormat;
 import org.apache.kafka.clients.admin.Admin;
 import picocli.CommandLine;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -17,6 +21,9 @@ import java.util.List;
  */
 @CommandLine.Command(name = "describe")
 public class DescribeTopicCommand extends BasicTopicCommand {
+
+    @CommandLine.Option(names = {"--output", "-o"}, defaultValue = "plain", description = "Output format supports: ${COMPLETION-CANDIDATES}")
+    private OutputFormat outputFormat;
 
     @Override
     public Integer call() {
@@ -29,13 +36,22 @@ public class DescribeTopicCommand extends BasicTopicCommand {
      */
     private Integer describeTopics() {
         try (Admin admin = Admin.create(AdminProperties.adminProperties(this.bootstrapServer))) {
-            admin.describeTopics(List.of(this.getTopicPrefixOrName())).topicNameValues().forEach((key, value) -> {
+            List<KafkaTopicDescription> kafkaTopicDescriptionList = new LinkedList<>();
+
+            // parse response from java admin
+            admin.describeTopics(this.getListOfTopicNames()).topicNameValues().forEach((key, value) -> {
                 try {
-                    System.out.println(value.get());
+                    kafkaTopicDescriptionList.add(KafkaTopicDescription.parseKafkaTopicDescription(value.get()));
                 } catch (Exception e) {
-                    throw new RuntimeException("Unable to describe topic: " + this.getTopicPrefixOrName() + " due: " + e.getCause());
+                    throw new RuntimeException("Unable to describe topic: " + key + " due: " + e.getCause());
                 }
             });
+
+            // provide response
+            System.out.println(DescribeTopicsUtils.getOutput(outputFormat, kafkaTopicDescriptionList));
+
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to describe topics due: " + e);
         }
         return 0;
     }
---
+++ admin/src/main/java/io/strimzi/models/KafkaTopicDescription.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.strimzi.models;
+
+import org.apache.kafka.clients.admin.TopicDescription;
+
+public record KafkaTopicDescription(String name, int partitionCount, int replicaCount) {
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPartitionCount() {
+        return partitionCount;
+    }
+
+    public int getReplicaCount() {
+        return replicaCount;
+    }
+
+    /**
+     * Extracts the topic name, partition count, and replica count from the {@code adminClientTopicDescription} parameter.
+     * consequently creating {@link KafkaTopicDescription} object.
+     *
+     * @param adminClientTopicDescription The topic description object obtained from Kafka's AdminClient.
+     * @return A new {@link KafkaTopicDescription} object that contains the name of the topic, the number of partitions
+     *         in the topic, and the number of replicas per partition.
+     */
+    public static KafkaTopicDescription parseKafkaTopicDescription(final TopicDescription adminClientTopicDescription) {
+        final int partitionCount = adminClientTopicDescription.partitions().size();
+        // as each partition has the same size, replica count is deduced from first partition
+        final int replicaCount = adminClientTopicDescription.partitions().get(0).replicas().size();
+        final String name = adminClientTopicDescription.name();
+        return new KafkaTopicDescription(name, partitionCount, replicaCount);
+    }
+}
