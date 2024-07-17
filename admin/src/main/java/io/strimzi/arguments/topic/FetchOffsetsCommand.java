01c3f53aed82b0bbcd188c8d219f196b0dbfa1ce
Jakub Stejskal <xstejs24@gmail.com>
2024-07-17 14:45:28 +0200

    Comments from Jan
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- admin/src/main/java/io/strimzi/arguments/topic/FetchOffsetsCommand.java
+++ admin/src/main/java/io/strimzi/arguments/topic/FetchOffsetsCommand.java
@@ -32,10 +32,10 @@ import java.util.Objects;
 @CommandLine.Command(name = "fetch-offsets")
 public class FetchOffsetsCommand extends BasicCommand {
 
-    @CommandLine.Option(names = {"--topic", "-t"}, description = "Name for topic to be inspected.", required = true)
+    @CommandLine.Option(names = {"--topic", "-t"}, description = "Name of the topic to be inspected.", required = true)
     String topicName;
 
-    @CommandLine.Option(names = {"--timestamp", "--time"}, description = "Timestamp of the offsets before that. See kafka-get-offsets tool for more info.")
+    @CommandLine.Option(names = {"--timestamp", "--time"}, description = "Timestamp marking the cutoff point before which offsets will be fetched. See kafka-get-offsets tool for more info.")
     String timestamp = "latest";
 
     @CommandLine.Option(names = {"--output", "-o"}, defaultValue = "plain", description = "Output format supports: ${COMPLETION-CANDIDATES}")
