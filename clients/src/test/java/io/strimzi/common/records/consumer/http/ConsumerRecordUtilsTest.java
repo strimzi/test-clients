fe9a1c3a2f6b81131a4570b4c979eb56dbfd4082
Lukas Kral <lukywill16@gmail.com>
2024-08-05 18:16:39 +0200

    Handle timestamp value in response from Bridge
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- clients/src/test/java/io/strimzi/common/records/consumer/http/ConsumerRecordUtilsTest.java
+++ clients/src/test/java/io/strimzi/common/records/consumer/http/ConsumerRecordUtilsTest.java
@@ -14,7 +14,25 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class ConsumerRecordUtilsTest {
 
     @Test
-    void testParseConsumerRecordsFromJson() throws JsonProcessingException {
+    void testParseConsumerRecordsWithTimestampFromJson() throws JsonProcessingException {
+        String response = "[{\"topic\":\"random-topic\",\"key\":\"key-0\",\"value\":\"Hello world-0\",\"partition\":0,\"offset\":0,\"timestamp\":\"1722874490\"}]";
+
+        ConsumerRecord expectedResult = new ConsumerRecord();
+        expectedResult.setPartition(0);
+        expectedResult.setOffset(0);
+        expectedResult.setTopic("random-topic");
+        expectedResult.setKey("key-0");
+        expectedResult.setValue("Hello world-0");
+        expectedResult.setTimestamp(1722874490L);
+
+        ConsumerRecord[] result = ConsumerRecordUtils.parseConsumerRecordsFromJson(response);
+
+        assertThat(result.length, is(1));
+        assertThat(result[0], is(expectedResult));
+    }
+
+    @Test
+    void testParseConsumerRecordsWithoutTimestampFromJson() throws JsonProcessingException {
         String response = "[{\"topic\":\"random-topic\",\"key\":\"key-0\",\"value\":\"Hello world-0\",\"partition\":0,\"offset\":0}]";
 
         ConsumerRecord expectedResult = new ConsumerRecord();
