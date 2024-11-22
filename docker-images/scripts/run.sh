3fb1f60289047571ef70b9a4e9c3762f7156d71b
Mickael Maison <mickael.maison@gmail.com>
2024-11-22 11:16:06 +0100

    Allow updating the classpath when starting clients
    Signed-off-by: Mickael Maison <mickael.maison@gmail.com>

--- docker-images/scripts/run.sh
+++ docker-images/scripts/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set +x
 
+JAR=$1
+MAIN=$2
+
 # In case we want to use producer, consumer, or streams app in K8s Job, we want to run test-clients jar
 # otherwise we don't want to run anything and let user to use admin-client CLI tool
 if [[ ! -z "${CLIENT_TYPE}" ]]; then
@@ -26,8 +29,10 @@ if [[ ! -z "${CLIENT_TYPE}" ]]; then
       export OAUTH_SSL_TRUSTSTORE_LOCATION=/tmp/oauth-truststore.p12
   fi
 
+  export CLASSPATH="${JAR}:${CLASSPATH}"
+
   # Make sure that we use /dev/urandom
   JAVA_OPTS="${JAVA_OPTS} -Dvertx.cacheDirBase=/tmp -Djava.security.egd=file:/dev/./urandom"
 
-  exec java $JAVA_OPTS -jar $JAR "$@"
+  exec java $JAVA_OPTS -cp $CLASSPATH $MAIN
 fi
