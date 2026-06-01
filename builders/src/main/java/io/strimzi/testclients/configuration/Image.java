22b1f4d311912ea972620e261a32f5b44513f814
Lukas Kral <lukywill16@gmail.com>
2026-06-01 10:23:15 +0200

    Add Kafka 4.3.0 and 4.2.1, remove 4.1.x
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- builders/src/main/java/io/strimzi/testclients/configuration/Image.java
+++ builders/src/main/java/io/strimzi/testclients/configuration/Image.java
@@ -9,7 +9,7 @@ import io.sundr.builder.annotations.Buildable;
 @Buildable(editableEnabled = false)
 public class Image {
     public static final String STRIMZI_TEST_CLIENTS_DEFAULT_IMAGE_ENV = "STRIMZI_TEST_CLIENTS_DEFAULT_IMAGE";
-    public static final String TEST_CLIENTS_DEFAULT_IMAGE = "quay.io/strimzi-test-clients/test-clients:latest-kafka-4.2.0";
+    public static final String TEST_CLIENTS_DEFAULT_IMAGE = "quay.io/strimzi-test-clients/test-clients:latest-kafka-4.3.0";
 
     private String imageName = Environment.getEnvOrDefault(STRIMZI_TEST_CLIENTS_DEFAULT_IMAGE_ENV, TEST_CLIENTS_DEFAULT_IMAGE);
     private String imagePullPolicy = "IfNotPresent";
